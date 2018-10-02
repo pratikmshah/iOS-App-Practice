11b29f5b736a781a819f43199b8716495ca60dec
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-02 16:45:21 -0500

    Change dice image based on random number rolled

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
 
@@ -26,6 +28,9 @@ class ViewController: UIViewController {
         // create a new randomly genereated integer from 0-5 inclusive
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
     }
     
