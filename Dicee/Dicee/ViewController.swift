bcace3c33d2c1bca0139758df487616c4b7cb580
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-02 17:17:56 -0500

    Add motion capability to app

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -20,18 +20,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        // create a new randomly genereated integer from 0-5 inclusive
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
     
 }
