5b9a8f51e701dbccc1af9872420bb8c62ca63ff4
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-02 14:23:33 -0500

    Generate random integers for each dice

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -19,6 +22,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // create a new randomly genereated integer from 0-5 inclusive
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
     }
     
 }
