8cbc6ba69d86bb2638c0263568b8aad267f99131
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-03 09:49:27 -0500

    Import icons and images

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Pratik Shah on 10/3/18.
+//  Copyright © 2018 Pratik Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
