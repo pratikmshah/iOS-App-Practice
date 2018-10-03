42d1696d000b8c15a30fbe9c1684051127addcef
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-03 11:16:32 -0500

    Link up magic ball image and button to code

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var magicBallImage: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+    }
 }
 
