f7143ca78d235df8c8e578b3333085da9f71e015
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-05 19:54:01 -0500

    Final commit

--- BMI.playground/Contents.swift
+++ BMI.playground/Contents.swift
@@ -3,10 +3,18 @@ import UIKit
 func calculateBMI(weightPounds: Double, heightInFeet: Double, heightInInches: Double) {
     let height = (heightInFeet * 12 + heightInInches)
     let BMI = weightPounds * 703 / pow(height, 2)
+    let shortBMI = String(format: "%.2f", BMI)
     var result = ""
     
-    if BMI > 
+    if BMI > 25 {
+        result = "overweight"
+    } else if BMI < 18.5 {
+        result = "underweight"
+    } else {
+        result = "normal"
+    }
     
+    print("Your BMI is \(shortBMI) and you are \(result)")
 }
 
 
