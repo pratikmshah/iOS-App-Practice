541e6d0b0cd2fe9d4fb5bf99d215dd04af9f53d9
Pratik Shah <pratik.t.shah@gmail.com>
2018-10-05 19:48:14 -0500

    Calculate BMI function and variables

---
+++ BMI.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func calculateBMI(weightPounds: Double, heightInFeet: Double, heightInInches: Double) {
+    let height = (heightInFeet * 12 + heightInInches)
+    let BMI = weightPounds * 703 / pow(height, 2)
+    var result = ""
+    
+    if BMI > 
+    
+}
+
+
+calculateBMI(weightPounds: 230.0, heightInFeet: 5, heightInInches: 7)
